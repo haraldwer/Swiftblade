5f9fb6fad6f32fba8cc4bb83b568de4610b8c69c
Harald Wergeland <harwer009@gmail.com>
2025-08-29 16:11:41 +0200

    docker works

--- Nakama/data/modules/main.lua
+++ Nakama/data/modules/main.lua
@@ -1,17 +1,17 @@
 
 -- Globals
 nk = require("nakama")
-utility = require("utility/utility")
-storage = require("utility/storage")
-map = require("utility/map")
-lb = require("utility/lb")
+utility = require("utility.utility")
+storage = require("utility.storage")
+map = require("utility.map")
+lb = require("utility.lb")
 
 -- rpcs
-local rpc_play = require("rpcs/rpc_play")
-local rpc_submit = require("rpcs/rpc_submit")
-local rpc_user = require("rpcs/rpc_user")
-local rpc_user_info = require("rpcs/rpc_user_info")
-local rpc_level_info = require("rpcs/rpc_level_info")
-local rpc_level_list = require("rpcs/rpc_level_list")
+local rpc_play = require("rpcs.rpc_play")
+local rpc_submit = require("rpcs.rpc_submit")
+local rpc_user = require("rpcs.rpc_user")
+local rpc_user_info = require("rpcs.rpc_user_info")
+local rpc_level_info = require("rpcs.rpc_level_info")
+local rpc_level_list = require("rpcs.rpc_level_list")
 
 nk.logger_info("Lua module loaded")
